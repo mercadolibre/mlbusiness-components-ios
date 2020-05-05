a2e129c87b6b71fbeeb0b37cdb47403cfa6bff93
Vicente Veltri <vicente.veltri@mercadolibre.com>
2020-05-05 19:24:23 -0300

    getAdditionalEdgeInsets

--- Source/Components/Touchpoints/MLBusinessTouchpointsBaseView.swift
+++ Source/Components/Touchpoints/MLBusinessTouchpointsBaseView.swift
@@ -21,6 +21,7 @@ protocol MLBusinessTouchpointsViewProtocol: class {
 
 class MLBusinessTouchpointsBaseView: UIView & MLBusinessTouchpointsViewInitializable & MLBusinessTouchpointsViewUpdatable {
     weak var delegate: MLBusinessTouchpointsViewProtocol?
+    var topConstraint, leftConstraint, bottomConstraint, rightConstraint: NSLayoutConstraint?
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
@@ -32,6 +33,15 @@ class MLBusinessTouchpointsBaseView: UIView & MLBusinessTouchpointsViewInitializ
     }
 
     func update(with _: Codable?) {}
+    
+    func setAdditionalEdgeInsets(with insets: [String : Any]?) {
+        guard let additionalEdgeInsets = insets else { return }
+        
+        topConstraint?.constant = CGFloat(additionalEdgeInsets["top"] as? NSNumber ?? 0)
+        leftConstraint?.constant = CGFloat(additionalEdgeInsets["left"] as? NSNumber ?? 0)
+        bottomConstraint?.constant = -CGFloat(additionalEdgeInsets["bottom"] as? NSNumber ?? 0)
+        rightConstraint?.constant = -CGFloat(additionalEdgeInsets["right"] as? NSNumber ?? 0)
+    }
     
     func getVisibleItems() -> [Trackable]? { return nil }
 }
