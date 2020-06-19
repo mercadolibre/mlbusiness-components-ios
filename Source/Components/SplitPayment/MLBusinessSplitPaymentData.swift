a7dbb5c0f0ed08aec4d9632f396126f47511c1cd
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-06-19 14:16:21 -0300

    Add SplitPaymentData protocol

---
+++ Source/Components/SplitPayment/MLBusinessSplitPaymentData.swift
@@ -0,0 +1,18 @@
+//
+//  MLBusinessSplitPaymentData.swift
+//  MLBusinessComponents
+//
+//  Created by Esteban Adrian Boffa on 19/06/2020.
+//
+
+import Foundation
+
+@objc public protocol MLBusinessSplitPaymentData: NSObjectProtocol {
+    @objc func getTitle() -> String
+    @objc func getTitleColor() -> String
+    @objc func getTitleBackgroundColor() -> String
+    @objc func getTitleWeight() -> String
+    @objc func getImageUrl() -> String
+    @objc func getButtonTitle() -> String
+    @objc func getButtonDeepLink() -> String
+}
