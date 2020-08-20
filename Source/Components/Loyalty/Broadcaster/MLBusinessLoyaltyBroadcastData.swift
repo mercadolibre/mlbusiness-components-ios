ac69700e64c685e2fa9501baa375ca7c22555530
Guido Maceira <guido.maceira@mercadolibre.com>
2020-08-20 15:09:20 -0300

    Suggested change

--- Source/Components/Loyalty/Broadcaster/MLBusinessLoyaltyBroadcastData.swift
+++ Source/Components/Loyalty/Broadcaster/MLBusinessLoyaltyBroadcastData.swift
@@ -9,7 +9,7 @@
 public class MLBusinessLoyaltyBroadcastData: NSObject {
 
     public let level: Int
-    public let percentage: Double
+    public let percentage: Float
     public let primaryColor: String
 
     public init(level: Int, percentage: Double, primaryColor: String) {
