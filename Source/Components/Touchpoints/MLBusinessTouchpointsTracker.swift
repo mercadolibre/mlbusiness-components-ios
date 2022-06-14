86c27d5da48b59442470fa6ed4e722f1ccb63a62
lbonasoraml <lautaro.bonasora@mercadolibre.com>
2022-06-14 17:23:28 -0300

    fix touchpoint

--- Source/Components/Touchpoints/MLBusinessTouchpointsTracker.swift
+++ Source/Components/Touchpoints/MLBusinessTouchpointsTracker.swift
@@ -34,7 +34,7 @@ class MLBusinessTouchpointsTracker: MLBusinessTouchpointsTrackerProtocol {
         var pendingPrints = [Trackable]()
 
         let filteredItems = items.filter { trackable in
-            guard let trackingId = trackable.trackingId, let _ = trackable.eventData else { return false }
+            guard let trackingId = trackable.trackingId else { return false }
             return !tracked.contains("\(trackingId)")
         }
         filteredItems.forEach { trackable in
@@ -55,47 +55,21 @@ class MLBusinessTouchpointsTracker: MLBusinessTouchpointsTrackerProtocol {
     
     //TAP
     func trackTap(item: Trackable) {
-//        track(items: [item], action: "tap")
-        guard let eventData = item.eventData else { return }
+        let eventData = item.eventData ?? MLBusinessCodableDictionary.init(value: [:])
         track(eventData: eventData.rawValue, action: "tap")
     }
-    
-//    private func track(items: [Trackable], action: String) {
-//        var eventData = [String : Any]()
-//        var touchpointTypeData = [String : Any]()
-//        let touchpointsType = touchpointsData.getTouchpointType()
-//        var itemsToTrack = [[String: Any]]()
-//        var itemToTrack = [String: Any]()
-//
-//        items.forEach { item in
-//            item.eventData?.keys.forEach { itemToTrack[$0] = item.eventData?[$0] }
-//            itemsToTrack.append(itemToTrack)
-//        }
-//        touchpointTypeData["items"] = itemsToTrack
-//
-//        if let touchpointTracking = touchpointsData.getTouchpointTracking?() {
-//            touchpointTracking.forEach { item in
-//                eventData[item.key] = item.value
-//            }
-//        }
-//
-//        eventData[touchpointsType] = touchpointTypeData
-//
-//        print("TRACKING EVENT - ACTION: \(action) - EVENTDATA: \(eventData)")
-//        trackingProvider?.track(action: action, eventData: eventData)
-//    }
     
     private func track(items: [Trackable], action: String) {
         var eventData = [String : Any]()
         var itemsToTrack = [[String: Any]]()
         var itemToTrack = [String: Any]()
         
-        items.forEach { item in
+        for item in items {
             item.eventData?.keys.forEach { itemToTrack[$0] = item.eventData?[$0] }
             itemsToTrack.append(itemToTrack)
         }
+        
         eventData["items"] = itemsToTrack
-
         track(eventData: eventData, action: action)
     }
     
