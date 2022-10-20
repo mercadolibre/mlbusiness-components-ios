e22b6c83e96f96bbbff6d5607bc82a5e489ab90f
Flavio Gomez <flavio.gomez@mercadolibre.com>
2022-10-20 18:58:26 -0300

    delete extra method

--- Source/Core/Extensions/UILabel+LineSpacing.swift
+++ Source/Core/Extensions/UILabel+LineSpacing.swift
@@ -9,38 +9,26 @@ import Foundation
 import UIKit
 
 extension UILabel {
-    func setLineSpacing(_ lineSpacing: CGFloat, maximumLineHeight: CGFloat) {
-        let text = self.text
-        if let text = text {
-            let attributeString = NSMutableAttributedString(string: text)
-            let style = NSMutableParagraphStyle()
-            
-            style.lineSpacing = lineSpacing
-            style.maximumLineHeight = maximumLineHeight
-            attributeString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attributeString.length))
-            self.attributedText = attributeString
-        }
-    }
-    
     func setLineHeight(_ lineHeight: CGFloat, align: NSTextAlignment = .left, lineSpacing: CGFloat = 0, lineBreakMode: NSLineBreakMode? = nil) {
-        if text == nil { return }
-        let attributedString = getAttributedString()
+        if text != nil {
+            let attributedString = getAttributedString()
 
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.maximumLineHeight = lineHeight
-        paragraphStyle.minimumLineHeight = lineHeight
-        paragraphStyle.alignment = align
-        paragraphStyle.lineSpacing = lineSpacing
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.maximumLineHeight = lineHeight
+            paragraphStyle.minimumLineHeight = lineHeight
+            paragraphStyle.alignment = align
+            paragraphStyle.lineSpacing = lineSpacing
 
-        if let lineBreakMode = lineBreakMode {
-            paragraphStyle.lineBreakMode = lineBreakMode
+            if let lineBreakMode = lineBreakMode {
+                paragraphStyle.lineBreakMode = lineBreakMode
+            }
+
+            attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                          value: paragraphStyle,
+                                          range: NSRange(location: 0, length: attributedString.length))
+
+            attributedText = attributedString
         }
-
-        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
-                                      value: paragraphStyle,
-                                      range: NSRange(location: 0, length: attributedString.length))
-
-        attributedText = attributedString
     }
 
     func getAttributedString() -> NSMutableAttributedString {
