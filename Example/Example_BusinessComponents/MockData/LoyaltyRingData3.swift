40fe90124fcd371b61a74443bec77790b1897240
Dario Gaspar <dario.gaspar@mercadolibre.com>
2023-05-05 15:34:38 -0300

    html

---
+++ Example/Example_BusinessComponents/MockData/LoyaltyRingData3.swift
@@ -0,0 +1,34 @@
+//
+//  LoyaltyRingData2.swift
+//  Example_BusinessComponents
+//
+//  Created by Dario Facundo Gaspar on 03/05/2023.
+//  Copyright © 2023 Mercado Libre. All rights reserved.
+//
+
+import UIKit
+import MLBusinessComponents
+
+class LoyaltyRingData3: NSObject, MLBusinessLoyaltyRingData {
+    
+    func getTitle() -> String {
+        return "Una vez que se acredite tu pago, ganarás 20 puntos."
+    }
+    
+    func getSubtitle() -> String {
+        return "Una vez que se acredite tu pago, ganarás 20 puntos."
+    }
+    
+    func getButtonTitle() -> String {
+        return "Ver mis beneficios"
+    }
+    
+    func getButtonDeepLink() -> String {
+        return "mercadopago://home"
+    }
+    
+    func getImageUrl() -> String{
+        return "https://i.ibb.co/z2Mdv12/Captura-de-pantalla-2023-05-04-a-la-s-14-38-25.jpg"
+    }
+}
+
