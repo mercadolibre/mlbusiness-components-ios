60a00082d5ef50cdfd78d9f57d4237ea8f435335
DarioGML <dario.gaspar@mercadolibre.com>
2023-07-05 12:05:54 -0300

    Add img in congrats component  (#192)
    * added img
    
    * html
    
    * html
    
    * html
    
    * html
    
    * bold method
    
    * hidde img y ring
    
    * rename
    
    * rename
    
    * rename
    
    * setting
    
    * refactor
    
    * refactor
    
    * nullable values
    
    * Visual improvements
    
    * improvements
    
    * Test case
    
    * Improvements
    
    * Constraints
    
    * Constraints changes
    
    * Improvements
    
    ---------
    
    Co-authored-by: Guido Maceira <guido.maceira@mercadolibre.com>

--- Source/Components/Loyalty/MLBusinessLoyaltyRingData.swift
+++ Source/Components/Loyalty/MLBusinessLoyaltyRingData.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 @objc public protocol MLBusinessLoyaltyRingData: NSObjectProtocol {
-    @objc func getRingHexaColor() -> String
-    @objc func getRingNumber() -> Int
-    @objc func getRingPercentage() -> Float
+    @objc optional func getRingHexaColor() -> String?
+    @objc optional func getRingNumber() -> NSNumber?
+    @objc optional func getRingPercentage() -> NSNumber?
     @objc func getTitle() -> String
-    @objc optional func getSubtitle() -> String
-    @objc func getButtonTitle() -> String
-    @objc func getButtonDeepLink() -> String
+    @objc optional func getSubtitle() -> String?
+    @objc optional func getButtonTitle() -> String?
+    @objc optional func getButtonDeepLink() -> String?
+    @objc optional func getImageUrl() -> String?
 }
