c049261c263516986c5de1401e8c238f373a20ad
Guido Maceira <guido.maceira@mercadolibre.com>
2023-07-10 11:25:03 -0300

    Update

--- Source/Components/LoyaltyHeader/MLBusinessLoyaltyHeaderData.swift
+++ Source/Components/LoyaltyHeader/MLBusinessLoyaltyHeaderData.swift
@@ -4,9 +4,8 @@
 //
 //  Created by Nicolas Battelli on 16/09/2019.
 //
-​
 import UIKit
-​
+
 public protocol MLBusinessLoyaltyHeaderData {
     func getBackgroundHexaColor() -> String
     @available(*, deprecated, message: "Use optional getPrimaryHexaColor() instead")
@@ -26,31 +25,33 @@ public protocol MLBusinessLoyaltyHeaderData {
     func getTitle() -> String?
     func getSubtitle() -> String
 }
-​
+
 public extension MLBusinessLoyaltyHeaderData {
     func getPrimaryHexaColor() -> String { "" }
     func getPrimaryHexaColor() -> String? {
         let value: String = getPrimaryHexaColor()
         return value.isEmpty ? nil : value
     }
-​
+    
     func getSecondaryHexaColor() -> String { "" }
     func getSecondaryHexaColor() -> String? {
         let value: String = getSecondaryHexaColor()
         return value.isEmpty ? nil : value
     }
-​
+    
+    
     func getRingNumber() -> Int { 0 }
     func getRingNumber() -> Int? {
         let value: Int = getRingNumber()
         return value == 0 ? nil : value
     }
+    
     func getRingPercentage() -> Float { 0.0 }
     func getRingPercentage() -> Float? {
         let value: Float = getRingPercentage()
         return value == 0.0 ? nil : value
     }
-​
+
     func getTitle() -> String { "" }
     func getTitle() -> String? {
         let value: String = getTitle()
